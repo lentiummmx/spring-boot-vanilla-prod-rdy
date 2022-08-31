7628922bb090a339dbc1e233ed603acf7cd8c4d0
lentiummmx <lentium.mmx@gmail.com>
2022-08-30 20:17:36 -0500

    sprng-bt-vnll Committed file(s) to add Persistence Layer, updated from PostgreSQL to Yugabyte (v0.3.0)

--- src/main/java/mx/nuniez/xoco/spring/boot/vanilla/data/entity/ProductItem.java
+++ src/main/java/mx/nuniez/xoco/spring/boot/vanilla/data/entity/ProductItem.java
@@ -5,14 +5,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "product_item")
 public class ProductItem {
 
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "product_item_id_generator")
+    @SequenceGenerator(name = "product_item_id_generator", sequenceName = "product_item_id_seq", allocationSize = 1)
     private Long id;
 
+    @Column
     private String title;
 
 }
