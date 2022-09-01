813fe9bbfde28d1d3549d14548856dae0b0eedfa
lentiummmx <lentium.mmx@gmail.com>
2022-08-31 20:39:12 -0500

    sprng-bt-vnll Committed file(s) to add Swagger for documenting API (v0.9.0)

--- src/main/java/mx/nuniez/xoco/spring/boot/vanilla/SpringBootVanillaApplication.java
+++ src/main/java/mx/nuniez/xoco/spring/boot/vanilla/SpringBootVanillaApplication.java
@@ -2,8 +2,10 @@ package mx.nuniez.xoco.spring.boot.vanilla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SpringBootVanillaApplication {
 
     public static void main(String[] args) {
