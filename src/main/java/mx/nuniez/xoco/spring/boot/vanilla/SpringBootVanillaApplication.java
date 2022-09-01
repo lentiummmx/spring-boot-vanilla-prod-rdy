8e61461baa168b44e0291998bd628c79fcb7e72a
lentiummmx <lentium.mmx@gmail.com>
2022-09-01 11:45:57 -0500

    sprng-bt-vnll Committed file(s) to update Swagger configurtion, to add postman collection for testing also (v0.9.1)

--- src/main/java/mx/nuniez/xoco/spring/boot/vanilla/SpringBootVanillaApplication.java
+++ src/main/java/mx/nuniez/xoco/spring/boot/vanilla/SpringBootVanillaApplication.java
@@ -2,10 +2,8 @@ package mx.nuniez.xoco.spring.boot.vanilla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SpringBootVanillaApplication {
 
     public static void main(String[] args) {
--- src/main/java/mx/nuniez/xoco/spring/boot/vanilla/api/config/SwaggerConfiguration.java
+++ src/main/java/mx/nuniez/xoco/spring/boot/vanilla/api/config/SwaggerConfiguration.java
@@ -2,26 +2,51 @@ package mx.nuniez.xoco.spring.boot.vanilla.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
 
+@EnableWebMvc
+@EnableSwagger2
 @Configuration
 public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
+        List<Parameter> parameters = new ArrayList<>();
+
+        parameters.add(new ParameterBuilder().name("Authorization")
+                .description("JWT token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build());
+
+        parameters.add(new ParameterBuilder().name("Content-Type")
+                .description("Content-Type")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(true)
+                .build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .globalOperationParameters(parameters);
     }
 
     private ApiInfo apiInfo() {
